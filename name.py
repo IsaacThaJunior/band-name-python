69c5d21d53c82588f5ca5aa65a8e8b66f1ef2c69
IsaacThaJunior <isaacjunez669@gmail.com>
2022-07-08 08:12:44 +0100

    first commit

---
+++ name.py
@@ -0,0 +1,10 @@
+# Getting started building a band name generator
+
+print('Welcome to this amazing program')
+
+city_name = input('Which City did you grow up in?\n')
+
+pet_name = input('What is your pets name?\n')
+
+print(
+    f'According to the aforementioned information. Your band name is {city_name} {pet_name}')
