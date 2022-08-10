4d9d23e740e80faa5da22d69e32de61765549b0f
IsaacThaJunior <isaacjunez669@gmail.com>
2022-08-10 23:45:16 +0100

    created a hangman game in python

--- hangman.py
+++ hangman.py
@@ -1,21 +1,48 @@
-# Create a hangman game
+# Step 4
+
 import random
+from hangman_words import word_list
+from hangman_art import logo, stages
 
-word_list = ['ardvard', 'baboon', 'camel']
+end_of_game = False
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-choice = random.choice(word_list)
+# Create a variable called 'lives' to keep track of the number of lives left.
+lives = 6
 
-print(f'The chosen word is {choice}')
+print(logo)
 
-guess = input('Guess a letter: ')
-
+# Create blanks
 display = []
-for i in range(len(choice)):
-    display.append('_')
+for _ in range(word_length):
+    display += "_"
 
-for position in range(len(choice)):
-    letter = choice[position]
-    if guess == letter:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    # Check guessed letter
+    if guess in chosen_word:
+        print(f'You have already guessed {guess}.')
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f'{guess} is not in the word. Therefore you lose a life.')
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    # print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
+    print(stages[lives])
