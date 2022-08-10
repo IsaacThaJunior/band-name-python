7c05a35aa7950fe48cc4d3ef0a043ef88e680722
IsaacThaJunior <isaacjunez669@gmail.com>
2022-08-10 07:06:41 +0100

    commit

--- hangman.py
+++ hangman.py
@@ -3,5 +3,19 @@ import random
 
 word_list = ['ardvard', 'baboon', 'camel']
 
-print(random.choice(word_list))
+choice = random.choice(word_list)
 
+print(f'The chosen word is {choice}')
+
+guess = input('Guess a letter: ')
+
+display = []
+for i in range(len(choice)):
+    display.append('_')
+
+for position in range(len(choice)):
+    letter = choice[position]
+    if guess == letter:
+        display[position] = letter
+
+print(display)
