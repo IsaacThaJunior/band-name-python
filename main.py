8840100073c8ba4eca18aa99005c977f1674b6b2
IsaacThaJunior <isaacjunez669@gmail.com>
2022-09-08 23:17:12 +0100

    omoh classes ehn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -10,3 +11,8 @@ for question in question_data:
     new_question = Question(quiz, answer)
 
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
