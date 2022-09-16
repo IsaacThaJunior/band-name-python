b548aa4f1fccf880cee5482691c5569049fc23ef
IsaacThaJunior <isaacjunez669@gmail.com>
2022-09-16 10:36:52 +0100

    got questions from openDB for my app

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    quiz = question['text']
-    answer = question['answer']
+    quiz = question['question']
+    answer = question['correct_answer']
 
     new_question = Question(quiz, answer)
 
@@ -16,3 +16,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print('You have completed the quiz')
+print(f'Your final score was: {quiz.current_score}/{quiz.question_number}')
--- quiz_brain.py
+++ quiz_brain.py
@@ -24,3 +24,4 @@ class QuizBrain:
         print(f'The correct answer was: {correct_answer}.')
         print(
             f'Your current score is: {self.current_score}/{self.question_number}')
+        print('\n')
